bb33da03d27f15aad6f8cfa2838bea29b42a770a
ta2 <dragon5555555@gmail.com>
2020-02-29 19:00:34 +0900

    getメソッドに変更

--- app/app.js
+++ app/app.js
@@ -10,12 +10,27 @@ var express = require('express');
 // Expressのインスタンス appを定義
 var app = express();
 
-// useメソッドを使用
-app.use(function (req, res, next) {
+// getメソッドでルーティング 第一引数にパスを設定｜ルートを設定
+app.get('/', function (req, res, next) {
 
   // res.send 応答をクライアントに送信して要求
   return res.send('Hello World');
 });
+
+// getメソッドでルーティング 第一引数にパスを設定｜hogeディレクトリを設定
+app.get('/hoge', function (req, res, next) {
+
+  // res.send 応答をクライアントに送信して要求
+  return res.send('Hoge');
+});
+
+
+// // useメソッドを使用
+// app.use(function (req, res, next) {
+
+//   // res.send 応答をクライアントに送信して要求
+//   return res.send('Hello World');
+// });
 
 // node.jsに定義したhttpサーバーにExpressのインスタンスであるappを設置
 var server = http.createServer(app);
