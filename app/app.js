22d1b849e34143e907389d89069e843a258f125e
ta2 <dragon5555555@gmail.com>
2020-03-08 13:23:00 +0900

    passportを使って名前、PW、アバターのローカル認証登録機能を実装｜表示機能は未

--- app/app.js
+++ app/app.js
@@ -19,11 +19,26 @@ var mongoose = require('mongoose');
 // multipart/form-data形式のリクエスト（画像などの大きなファイルリクエストをパースする形式）をパースするミドルウェア
 var fileUpload = require('express-fileupload');
 
-// スキーマをインポート
+// スキーマをインポート｜/schema/Message.js
 var Message = require('./schema/Message');
+
+// スキーマをインポート｜/schema/User.js
+var User = require('./schema/User');
+
+
+
+// express用 汎用認証ミドルウェア
+var passport = require('passport');
+
+// ローカル認証を行う
+var LocalStrategy = require('passport-local').Strategy;
+
+// セッションを扱う
+var session = require('express-session');
 
 // Expressのインスタンス appを定義
 var app = express();
+
 
 // 指定したパスにMongoDBを接続してコールバック関数を実行させる
 mongoose.connect('mongodb://localhost:27017/chatapp', function (err) {
@@ -36,7 +51,6 @@ mongoose.connect('mongodb://localhost:27017/chatapp', function (err) {
 });
 
 
-
 // 以下、このサイトを参考に書いた記法 ちゃんと動く！ https://reffect.co.jp/node-js/express-jsnode-js-mongodb 
 
 // mongoose.connect('mongodb://localhost:27017/chatapp');
@@ -47,21 +61,38 @@ mongoose.connect('mongodb://localhost:27017/chatapp', function (err) {
 
 // db.once('open', () => console.log('DB connection successful'));
 
+
 // リクエストを返すルーティングよりも前の位置でbody-paserミドルウェアを設定する（そうしないと適用されなくなってしまう）
 app.use(bodyparser());
 
-// 指定したパスから静的ファイルを配信
-app.use("/image", express.static(path.join(__dirname, 'image')));
 
-// テンプレートエンジンを設定
+// セッションをセット
+app.use(session({ secret: 'HogeFuga' }));
+
+// 認証用ミドルウェア passportを初期化
+app.use(passport.initialize());
+
+// パスポートにセッションを指定
+app.use(passport.session());
+
+
+// 指定したパスにテンプレートエンジンを設定
 app.set('views', path.join(__dirname, 'views'));
 
 // pugをセット
 app.set('view engine', 'pug');
 
 
+// 指定したパスから静的ファイルを配信｜画像ファイル
+app.use("/image", express.static(path.join(__dirname, 'image')));
 
-// getメソッドでルーティング 第一引数にパスを設定｜ルート(index)を設定
+// 指定したパスから静的ファイルを配信｜アバターアイコン
+app.use('/avatar', express.static(path.join(__dirname, 'avatar')));
+
+
+
+// getメソッドでルーティング 第一引数にパスを設定(/の記号を必ず入れること！）｜ルート(index)を設定
+// ルーティングを作成｜トップ画面を描画
 app.get('/', function (req, res, next) {
 
   // find()｜クエリに該当する複数のデータを取得するメソッド
@@ -71,15 +102,132 @@ app.get('/', function (req, res, next) {
     if (err) throw err;
 
     // res.renderでテンプレートでの描画を設定｜テンプレート名, テンプレート内で指定している変数の値をセット
-    return res.render('index', { messages: msgs });
+    return res.render('index', {
+      messages: msgs
+    });
 
   });
 });
 
-// getメソッドでルーティング 第一引数にパスを設定｜updateディレクトリを設定
+
+// ルーティングを作成｜会員登録用画面
+app.get('/signin', function (req, res, next) {
+  return res.render('signin');
+});
+
+
+// ルーティングを作成｜会員登録データ投稿画面
+app.post('/signin', fileUpload(), function (req, res, next) {
+
+  var avatar = req.files.avatar;
+  avatar.mv('./avatar/' + avatar.name, function (err) {
+
+    if (err) throw err;
+
+    var newUser = new User({
+      username: req.body.username,
+      password: req.body.password,
+      avatar_path: '/avatar/' + avatar.name
+    });
+
+    newUser.save((err) => {
+      if (err) throw err;
+      return res.redirect('/');
+    });
+  });
+});
+
+
+// passport.authenticate('local')でセッションが設定されていない場合に処理が渡される
+passport.use(new LocalStrategy(
+
+  // usernameとpasswordはPOST形式でルーティングの/loginに送信されたパラメータから自動的にセットされる
+  // まず、POSTで送信されたユーザー名とパスワードからDBに問い合わせて、該当のユーザーが登録済かをチェック、コールバック関数のdone()を呼びだす。
+  function (username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+
+      // doneの第一引数にはエラーが発生した場合にtrurと評価される値を渡す。第2引数には認証が成功して登録済みのユーザーの場合はtrueをそうでない場合はfalseを渡す。
+      if (err) { return done(err); }
+
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+
+      if (user.password !== password) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+
+      // doneを呼ぶことができるのは1回のリクエストのたびに1回だけ。認証に成功した場合はこのユーザー情報を使ってセッションIDを生成する。
+      return done(null, user);
+    });
+  }
+));
+
+// passportでセッションを管理するにあたって、cookieに保存され、ユーザーを区別するために使用するセッション情報に、データベースに保存されたユーザーの全ての情報を使うのは不都合が生じることがある。そのため、セッションのシリアライズとデシリアライズをおこなう。
+
+// シリアライズはpassport.use()で認証されたユーザーのデータから、どの情報をセッションに格納するかを決定する。ここではuser._idを指定している。
+
+// ※ 用語解説｜シリアライズ（serialize）｜プログラミングでオプジェクト化されたデータを、ファイルやストレージに保存したり、ネットワークで送受信したりできるような形に変換することを言います。
+
+// これはExpressのsession変数であるreq.sessionに格納され、req.session.passport.user = 'hogehoge'のようにセッションのペイロード（ヘッダなどの制御情報を除いた、正味のデータ本体のこと）として任意のルーティングで使用することができる。
+passport.serializeUser(function (user, done) {
+  done(null, user._id);
+});
+
+
+// デシリアライズは、ログイン済みのクライアントからセッション変数を送信されるたびに、その中身を検証するために送信するミドルウェア。通常 req.session.passport.userの中身がコールバック関数の第一引数として渡される。
+
+// ※ 用語解説｜デシリアライズ（deserialize）｜シリアライズされたデータをプログラミングで扱えるようにオブジェクトの型に復元すること
+
+// 通常このデータを使って、データベースなどから該当するユーザーの情報を取得するので、クエリとして使用可能な項目を指定する。
+passport.deserializeUser(function (id, done) {
+  User.findOne({ _id: id }, function (err, user) {
+    done(err, user);
+  });
+});
+
+
+
+
+// app.post('/avatar', fileUpload(), function (req, res, next) {
+
+//   var avatar = req.files.avatar;
+//   avatar.mv('./avatar/' + avatar.name, function (err) {
+
+//     if (err) throw err;
+
+//     var newUser = new User({
+//       username: req.body.username,
+//       password: req.body.password,
+//       avatar_path: '/avatar/' + avatar.name
+//     });
+
+//     newUser.save((err) => {
+//       if (err) throw err;
+//       return res.redirect('/');
+//     });
+//   });
+// });
+
+
+// ルーティングを作成｜ログイン画面を描画
+app.get('/login', function (req, res, next) {
+  return res.render('login');
+});
+
+// ログイン画面に認証を送信｜OKならトップ画面へ、失敗ならログイン画面へ飛ばす
+app.post('/login', passport.authenticate('local',
+  {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  })
+);
+
+
+// ルーティングを作成｜update画面を描画
 app.get('/update', function (req, res, next) {
 
-  // pugファイルを描画
+  // pugファイル｜update.pugを描画
   return res.render('update');
 });
 
